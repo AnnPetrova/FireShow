ff7973046b545aca706590231c2ea032330079dd
AnnPetrova <brigantinas_fan@mail.ru>
2016-10-28 13:23:31 +0300

    clients fixed, added button in carousel

--- js/index.js
+++ js/index.js
@@ -4,31 +4,63 @@ $(function () {
             dots: true,
             infinite: true,
             slidesToShow: 3,
-            slidesToScroll: 3
+            slidesToScroll: 3,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            centerMode: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
         });
-        $(".center").slick({
+       /* $(".center").slick({
             dots: true,
             infinite: true,
-            centerMode: true,
             slidesToShow: 3,
             slidesToScroll: 3
-        });
-        $(".variable").slick({
+        });*/
+       /* $(".variable").slick({
             dots: true,
             infinite: true,
             variableWidth: true
-        });
+        });*/
+        /*$(".autoplay").slick({
+        });*/
     });
 });
 
-$(function(){
+/*$(function(){
     $('.multiple-items').slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3
     });
-});
-
+});*/
+/*
 $(function(){
     $('.responsive').slick({
         dots: true,
@@ -65,4 +97,4 @@ $(function(){
             // instead of a settings object
         ]
     });
-})
+});*/
