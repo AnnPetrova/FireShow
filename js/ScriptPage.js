290a689b36f81d943dfe9533d0bf1140cc86c824
AnnPetrova <brigantinas_fan@mail.ru>
2016-10-31 12:54:41 +0300

    project button changed

--- js/ScriptPage.js
+++ js/ScriptPage.js
@@ -1,70 +1,62 @@
 var main = function () {
-
+    var flag2 = new Boolean(true);
 
     /*$('#donebut').click(function() {
-     var showcase = $("#showcase");
-     showcase.fadeIn( 1500 );
+     if(flag2) {
+     flag2 = false;
+     $('.wrap1').animate({'opacity' : '1'}, 500);
+     $('.content').animate({'opacity' : '0'}, 500);
+     //$('.wrap1').getElementsByTagName('a').css('display', 'block');
+     $('body').addClass('keke');
 
-     $('.container').removeClass('animated zoomIn');
-     $('.container').addClass('animated zoomOut');
-     $('.done').removeClass('animated zoomOut');
-     $('.done').addClass('animated zoomIn');
-     $('.done').css({"z-index":"2"});
-     $('.zoomIn').css({"-webkit-animation-delay": "1s",
-     "animation-delay": "1s"});
-     $('.zoomOut').css({"-webkit-animation-delay": "0s",
-     "animation-delay": "0s"});
+     } else {
+     flag2 = true;
+     $('.wrap1').animate({'opacity' : '0'}, 500);
+     $('.content').animate({'opacity' : '1'}, 500);
+     $('body').removeClass('keke');
 
-     })*/
+     $('body').css("backgroundImage", 'url(../images/05.jpg)');
 
-    /*$('#prevbut').click(function() {
-     $('.container').removeClass('animated zoomOut');
-     $('.container').addClass('animated zoomIn');
-     $('.wrap').removeClass('animated zoomIn');
-     $('.wrap').addClass('animated zoomOut');
-     //$('.done').css({"z-index":"0"});
-     //$('.donik').removeClass('animated zoomIn');
-     //$('.donik').addClass('animated zoomOut');
-     //$('.donik').css({"z-index":"0"});
-     $('.zoomIn').css({"-webkit-animation-delay": "1s",
-     "animation-delay": "1s"});
-     $('.zoomOut').css({"-webkit-animation-delay": "0s",
-     "animation-delay": "0s"});
+     }
+     return false;
      });*/
 
-    /*$('#donebut').click(function () {
-        if ($('.wrap1').css('display') == 'none'){
-            $('.wrap1').css('display','block');
-        }
-        else{
-            $('.wrap1').css('display','none');
-        }
-    });*/
+    /*  $( "#donebut" )
+     .mouseover(function() {
+     i += 1;
+     $( this ).find( "span" ).text( "mouse over x " + i );
+     })
+     .mouseout(function() {
+     $( this ).find( "span" ).text( "mouse out " );
+     });*/
+    var n = 0;
+    $("#donebut")
+        .mouseenter(function () {
+            n += 1;
+            /*$( this ).find( "span" ).text( "mouse enter x " + n );*/
+            flag2 = false;
+            $('.wrap1').animate({'opacity': '1'}, 500);
+            $('.content').animate({'opacity': '0'}, 500);
+            //$('.wrap1').getElementsByTagName('a').css('display', 'block');
+            $('body').addClass('keke');
 
+            return false;
+        })
+        .mouseleave(function () {
+            /*$( this ).find( "span" ).text( "mouse leave" );*/
+        })
+        .click(function () {
+            if (!flag2) {
+                flag2 = true;
+                $('.wrap1').animate({'opacity': '0'}, 500);
+                $('.content').animate({'opacity': '1'}, 500);
+                $('body').removeClass('keke');
 
+                $('body').css("backgroundImage", 'url(../images/05.jpg)');
 
-var flag2 = new Boolean(true);
-
-$('#donebut').click(function() {
-  if(flag2) {
-    flag2 = false;
-    $('.wrap1').animate({'opacity' : '1'}, 500);
-    $('.content').animate({'opacity' : '0'}, 500);
-    //$('.wrap1').getElementsByTagName('a').css('display', 'block');
-    $('body').addClass('keke');
-
-  } else {
-    flag2 = true;
-    $('.wrap1').animate({'opacity' : '0'}, 500);
-    $('.content').animate({'opacity' : '1'}, 500);
-    $('body').removeClass('keke');
-
-    $('body').css("backgroundImage", 'url(../images/05.jpg)');
-
-  }
-    return false;
-});
-
+            }
+            return false;
+        });
 
 };
 
